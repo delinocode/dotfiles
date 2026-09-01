1359b4dc04e8f1cbe0d418312bc0606e4cd17bf9
Abdelrahmane FERCHICHI <abdel.ferchi38@gmail.com>
2026-09-01 18:22:16 -0400

    Provide dotfiles path to Home Manager modules
    Pass dotfiles = ./. through extraSpecialArgs so modules/files.nix can link repository-managed files.

--- flake.nix
+++ flake.nix
@@ -20,10 +20,11 @@
           ];
       };
       user = "delai";
+      dotfiles = ./.;
     in {
       homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        extraSpecialArgs = { inherit user; };
+        extraSpecialArgs = { inherit user dotfiles; };
         modules = [ ./home.nix ];
       };
     };
